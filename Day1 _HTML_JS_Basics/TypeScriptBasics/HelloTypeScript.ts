b0a76dfde0806220f6295d323c4592af9a9c8d3a
manikanta-annavarapu <manikanta.annavarapu95@gmail.com>
2019-11-25 14:32:46 +0530

    data types and declarations

--- Day1 _HTML_JS_Basics/TypeScriptBasics/HelloTypeScript.ts
+++ Day1 _HTML_JS_Basics/TypeScriptBasics/HelloTypeScript.ts
@@ -1,3 +1,14 @@
-var x = 10; //type inference
+// var x = 10; //type inference
+var x: number = 10; // type annotation
 // x = "Hi"; //throws and error
 console.log(x);
+
+var s: string;
+var o: object;
+var b: boolean;
+var y; //if we are not declareing any type then it will be having 'any' datatype;
+// var y: any; //alternative
+y = 10;
+y = true;
+y = "hello";
+y = [10,20,30,40];
