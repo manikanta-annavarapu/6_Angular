eb41c37a4d1aec39cb059231a624c0a9dfeba245
manikanta-annavarapu <manikanta.annavarapu95@gmail.com>
2019-11-26 15:08:11 +0530

    stringInterpolationAndPropertyBindingDOMattributes

--- Day2_Angular_Intro/firstcliapp/src/app/app.component.ts
+++ Day2_Angular_Intro/firstcliapp/src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firstcliapp';
+  imageUrl:string = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png";
   course1:any={name:"c#",price:250,duration:'1 week',city:'pune'};
   course2:any={name:"#c++",price:600,duration:'2 weeks',city:'Goa'};
   course3:any={name:"c",price:555,duration:'3 weeks',city:'Mumbai'};
