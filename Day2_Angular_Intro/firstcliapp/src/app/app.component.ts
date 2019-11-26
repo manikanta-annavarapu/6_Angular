04c967d6d7b48564c39ca9f5cd10ae36f9957369
manikanta-annavarapu <manikanta.annavarapu95@gmail.com>
2019-11-26 16:06:49 +0530

    structural directive *ngFor

--- Day2_Angular_Intro/firstcliapp/src/app/app.component.ts
+++ Day2_Angular_Intro/firstcliapp/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Course} from './course.model';
 export class AppComponent {
   title = 'firstcliapp';
   imageUrl:string = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png";
-  course1:Course=new Course("Java",234,'1 day',"pune");
-  course2:Course=new Course("#c++",600,'2 weeks','Goa');
-  course3:Course=new Course("c",555,'3 weeks','Mumbai');
+  courses:Course[]=[new Course("Java",234,'1 day',"pune"),
+  new Course("#c++",600,'2 weeks','Goa'),
+ new Course("c",555,'3 weeks','Mumbai')];
 }
