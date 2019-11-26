4d3d9bd2e4fb8fb6fd064d0135412f3a950d4393
manikanta-annavarapu <manikanta.annavarapu95@gmail.com>
2019-11-26 15:01:26 +0530

    practise

--- Day2_Angular_Intro/firstcliapp/src/app/app.component.ts
+++ Day2_Angular_Intro/firstcliapp/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firstcliapp';
-  course1:any={name:"c#",price:250};
-  course2:any={name:"#c++",price:600};
-  course3:any={name:"c",price:555};
+  course1:any={name:"c#",price:250,duration:'1 week',city:'pune'};
+  course2:any={name:"#c++",price:600,duration:'2 weeks',city:'Goa'};
+  course3:any={name:"c",price:555,duration:'3 weeks',city:'Mumbai'};
 }
--- Day2_Angular_Intro/firstcliapp/src/app/course.component.ts
+++ Day2_Angular_Intro/firstcliapp/src/app/course.component.ts
@@ -2,11 +2,11 @@ import {Component, Input} from '@angular/core';
 
 @Component({
     selector: 'course',
-    template: '<h2>{{details.name}}, {{details.price}}<h2>'
+    template: '<h2>{{details.name}}, {{details.price}}, {{details.duration}}, {{details.city}}<h2>'
 })
 
 export class CourseComponent{
   // @Input('courseName') name: string ="React" //here coursename is alias name from 'name' variable
   // @Input('price') price:number = 20000;
-  @Input('coursedetails') details:any={name:"React",price:21};
+  @Input('coursedetails') details:any={name:"React",price:21,duration:'1 week',city:'pune'};
 }
