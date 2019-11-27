917c7a18d0f1f6e6ffb82338304156a7181020ca
manikanta-annavarapu <manikanta.annavarapu95@gmail.com>
2019-11-27 12:26:30 +0530

    ngClass directive

--- OnlineShopping/src/app/product.component.ts
+++ OnlineShopping/src/app/product.component.ts
@@ -2,8 +2,10 @@ import {Component, Input} from '@angular/core';
 import { Product } from './product.model';
 @Component({
  selector:'product',
- templateUrl:'./product.component.html'
+ templateUrl:'./product.component.html',
+ styleUrls:['./product.style.css']
 })
 export class ProductComponent{
     @Input() product:Product;
+    isHighlighted:boolean=false;
 }
