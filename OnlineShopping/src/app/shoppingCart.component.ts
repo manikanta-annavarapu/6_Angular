16e66a7cf8f95808e1f5e1b2db6aab2c79362039
manikanta-annavarapu <manikanta.annavarapu95@gmail.com>
2019-11-27 11:32:57 +0530

    binding on click

--- OnlineShopping/src/app/shoppingCart.component.ts
+++ OnlineShopping/src/app/shoppingCart.component.ts
@@ -6,8 +6,15 @@ import {Product} from './product.model';
     templateUrl:'./shoppingCart.component.html'
 })
 export class ShoppingCartComponent{
+    heading:string="Online Shopping";
     products:Product[]=[new Product("Watch",1200,1,4,4,"https://images-na.ssl-images-amazon.com/images/I/71gdBQP%2BqGL._UL1500_.jpg"),
                 new Product("Phone",23123,1,4.5,4,"https://www.boostmobile.com/content/dam/boostmobile/en/products/phones/samsung/galaxy-s10e/white/device-front.png.transform/pdpCarousel/image.jpg"),
                 new Product("Bottle",50,10,3.2,10,"https://cdn11.bigcommerce.com/s-b0j8iiyjpx/products/112/images/642/LARQ_Bottle_Product-1_17oz_MB__25321.1571652469.1280.1280.jpg")
-        ]
+        ];
+
+
+        ChangeHeading(){
+            this.heading="Flipkart";
+            
+        }
 }
