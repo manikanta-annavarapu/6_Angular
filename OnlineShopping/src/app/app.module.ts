6d2311f5c9145f89fccc500c36a86b69ce2bceb2
manikanta-annavarapu <manikanta.annavarapu95@gmail.com>
2019-11-30 10:17:05 +0530

    child routes

--- OnlineShopping/src/app/app.module.ts
+++ OnlineShopping/src/app/app.module.ts
@@ -18,19 +18,36 @@ import { ProductService } from './product.service';
 import {Routes, RouterModule} from '@angular/router';
 import { PostdetailsComponent } from './postdetails/postdetails.component'
 import { PostsService } from './posts/posts.service';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Route } from '@angular/compiler/src/core';
 
-const routes: Routes= [
-    {path:"",component:ShoppingCartComponent},
-    {path:"posts",component:PostsComponent},
-    {path:"company", component:CompanyComponent},
-    {path:"posts/details/:id", component:PostdetailsComponent},
-    {path:"newproduct", component:NewproductComponent},
-    {path:"**",redirectTo:"/"}
+
+// const routes: Routes= [
+//     {path:"",component:ShoppingCartComponent},
+//     {path:"posts",component:PostsComponent},
+//     {path:"company", component:CompanyComponent},
+//     {path:"posts/details/:id", component:PostdetailsComponent},
+//     {path:"newproduct", component:NewproductComponent},
+//     {path:"**",redirectTo:"/"}
+// ];
+
+const routes :Routes =[
+  {path:'',component:LoginComponent},
+  {path:'dashboard',component :DashboardComponent, children: [
+    {path:'',component:ShoppingCartComponent},
+    {path:'posts', component:PostsComponent},
+    {path:'posts/details/:id',component:PostdetailsComponent},
+    {path:'newproduct', component:NewproductComponent},
+    {path:'company', component: CompanyComponent}
+  ]},
+  {path:'**', redirectTo:'/'}
 ];
 
 @NgModule({
   declarations: [
-    AppComponent, ShoppingCartComponent, ProductComponent,QuantityPipe, CompanyComponent, PostsComponent, TodosComponent, NewproductComponent, PostdetailsComponent
+    AppComponent, ShoppingCartComponent, ProductComponent,QuantityPipe, 
+    CompanyComponent, PostsComponent, TodosComponent, NewproductComponent, PostdetailsComponent, LoginComponent, DashboardComponent
   ],
   imports: [
     BrowserModule,FormsModule,HttpClientModule,
